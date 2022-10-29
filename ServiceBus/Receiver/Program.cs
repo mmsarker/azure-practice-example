06978a81d8015a40ba7782cc00ceaf1035b2b26f
Mizan <mizanur.rahman@selise.ch>
2022-10-29 13:03:37 +0600

    Service bus example created

---
+++ ServiceBus/Receiver/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+public partial class Program{
+    static string _connectionString = "Endpoint=sb://test-service-bus-namespace-11.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GJ66J0fRQg0aCV0uWGHsmbmW2X9pGMAw253EnVfJ2hk=";
+    static string _queueName = "test-queue-name-11";
+
+    static ServiceBusClient _serviceBusClient;
+    static ServiceBusProcessor _serviceBusProcessor;
+    
+    public static async Task Main()
+    {
+        try
+        {
+            _serviceBusClient = new ServiceBusClient(_connectionString);
+            _serviceBusProcessor = _serviceBusClient.CreateProcessor(_queueName, new ServiceBusProcessorOptions());
+            _serviceBusProcessor.ProcessMessageAsync += MessageHandler;
+            _serviceBusProcessor.ProcessErrorAsync += ErrorHandler;
+            await _serviceBusProcessor.StartProcessingAsync();
+            Console.WriteLine("Message is processing. press any key to stop.");
+            Console.ReadKey();
+
+            Console.WriteLine("Stopping to receive messages.");
+            await _serviceBusProcessor.StopProcessingAsync();
+        }
+        catch(Exception ex)   
+        {
+            await _serviceBusProcessor.DisposeAsync();
+            await _serviceBusClient.DisposeAsync();
+        }
+
+    }
+
+    static async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string messageBody = args.Message.Body.ToString();
+        Console.WriteLine($"Message received: {messageBody}");
+        await args.CompleteMessageAsync(args.Message);        
+    }
+
+    static Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+        return Task.CompletedTask;
+    }
+}
---
+++ ServiceBus/Sender/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+public partial class Program
+{
+    static string _connectionString = "Endpoint=sb://test-service-bus-namespace-11.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GJ66J0fRQg0aCV0uWGHsmbmW2X9pGMAw253EnVfJ2hk=";
+    static string _queueName = "test-queue-name-11";
+    static ServiceBusClient _client;
+    static ServiceBusSender _sender;
+    private const int _numberOfMessages = 3;
+
+
+    static async Task Main()
+    {
+        _client = new ServiceBusClient(_connectionString);
+        _sender = _client.CreateSender(_queueName);
+
+        using ServiceBusMessageBatch messageBatch = await _sender.CreateMessageBatchAsync();
+
+        for(int i = 1;i<=_numberOfMessages;i++)
+        {
+            if(!messageBatch.TryAddMessage(new ServiceBusMessage($"Message{i}")))
+            {
+                throw new Exception($"Exception {i} has occured. ");
+            }            
+        }
+
+        try
+        {            
+            await   _sender.SendMessagesAsync(messageBatch);
+            Console.WriteLine($"Messages sent: {_numberOfMessages}");
+        } 
+        finally
+        {
+            await _sender.DisposeAsync();
+            await _client.DisposeAsync();
+        }
+
+
+        Console.Write("This is test");
+        Console.ReadKey();
+    }
+}
