0534e80ee5fab312bd52a1e58d15f8d29cfa9612
Mizan <mizanur.rahman@selise.ch>
2022-10-30 00:15:42 +0600

    Storage queue creation

---
+++ StorageQueue/Sender/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Core;
+using Azure.Storage.Queues;
+using Azure.Storage;
+using Azure.Storage.Queues.Models;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        string connectionString = "";
+        string queueName = "";
+
+        QueueClient  queueClient = new QueueClient(connectionString, queueName);
+        queueClient.CreateIfNotExists();
+
+        if(queueClient.Exists())
+        {
+            string message = "this is a test message";
+            await queueClient.SendMessageAsync(message);
+        }        
+
+    }
+
+}
